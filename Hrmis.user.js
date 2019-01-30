ebc401b9597d0cdbb1124e93b686a85701581c5c
last2004 <46987991+last2004@users.noreply.github.com>
2019-01-30 15:11:55 +0800

    Update Hrmis.user.js

--- Hrmis.user.js
+++ Hrmis.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hrmis
 // @namespace    http://inner-portal.ccs.org.cn/hrmis/
-// @version      20190130-1
+// @version      20190131
 // @description  Just for fun!
 // @author       You
 // @match        http://inner-portal.ccs.org.cn/hrmis/exam/exam!examOnline.do?className=*
